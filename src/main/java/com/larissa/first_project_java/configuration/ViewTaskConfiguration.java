5649c3a0c4166f6c7e2362c988c64566b6b39bf4
larissacesarr <larissavitoriacesarcruz@gmail.com>
2025-03-20 07:00:24 -0300

    Integrando o backend com o forntend

--- src/main/java/com/larissa/first_project_java/configuration/ViewTaskConfiguration.java
+++
@@ -1,9 +0,0 @@
-package com.larissa.first_project_java.configuration;
-
-import org.springframework.context.annotation.Configuration;
-
-@Configuration
-public class ViewTaskConfiguration {
-
-
-}
---
+++ src/main/java/com/larissa/first_project_java/configuration/WebConfig.java
@@ -0,0 +1,21 @@
+package com.larissa.first_project_java.configuration;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("http://localhost:5173/")
+                .allowCredentials(true)
+                .allowedMethods("GET", "POST", "PUT", "DELETE")
+                .allowedHeaders("*");
+    }
+}
+
+
